05933f21e3226ca51948b15b7f64e2b501fd62e8
Andrey Arzumaniyan <88486829+AndrewArzumaniyan@users.noreply.github.com>
2023-04-13 22:33:00 +0600

    add solution

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import norm
 
 chat_id = 938988157 # Ваш chat ID, не меняйте название переменной
 
@@ -8,7 +8,14 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.02
+    
+    w = y_success / y_cnt
+    p0 = x_success / s_cnt
+    denom = (p0 * (1 - p0)) ** 0.5
+    
+    U = ((w - p0) * y_cnt**0.5) / denom
+    
+    Ucr = norm.ppf(1/2 - alpha
+    
+    return U < Ucr
